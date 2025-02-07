a74a4361527452bbd9c9b5c639030b5ba6cd8f81
ahmadalanib <alnaib888@gmail.com>
2025-02-07 15:11:42 +0100

    add twig

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,11 +2,24 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class HelloController
+class HelloController extends AbstractController
 {
-    public function index()
+    #[Route('/', name: 'hello')]
+    public function index(): Response
     {
-        return new Response('Hallo Welt!');
+        return new Response('Hallo dfdf!');
+    }
+   
+
+    #[Route('/{id}', name: 'hello_id')]
+    public function showOne($id): Response
+    {
+        return $this->render('hello/show_one.html.twig', [
+            'id' => $id,
+        ]);
+        // return new Response('Hallo ' . $id);
     }
 }
